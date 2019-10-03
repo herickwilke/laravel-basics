cfcf47d8db29e60865ece6384ddd1b2c9fcbcedc
Herick <herick@weecode.com.br>
2019-10-03 08:22:04 -0300

    Prosseguindo aula.

--- database/migrations/2019_10_01_145552_add_brand_to_products.php
+++ database/migrations/2019_10_01_145552_add_brand_to_products.php
@@ -27,7 +27,8 @@ class AddBrandToProducts extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropForeign(['brand_id']);
+            $table->dropColumn(['brand_id']);
         });
     }
 }
--- database/migrations/2019_10_02_200006_create_departments.php
+++ database/migrations/2019_10_02_200006_create_departments.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProducts extends Migration
+class CreateDepartments extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateProducts extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->integer('stock');
-            $table->double('price');
-            $table->string('description')->nullable();
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateProducts extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('departments');
     }
 }
